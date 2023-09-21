d1072c395f587d1a38353791e5b2eed3bf483c8c
adrian_skowron <adrian_skowron@MacBook-Pro-adrian-skowron.local>
2023-09-21 18:07:48 +0200

    Fix in count_elements and validate_password

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,13 @@
 class CountElements
+    def initialize(array)
+        @array = array
+    end
 
+    def count_elements(array)
+        @array.each_with_object(Hash.new(0)) { |element, hash| hash[element] += 1 }
+    end
+
+    def perform
+        count_elements(@array)
+    end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,31 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def validate
+      # Check length
+      return false unless @password.length.between?(6, 24)
+  
+      # Check for at least one uppercase letter
+      return false unless @password.match?(/[A-Z]/)
+  
+      # Check for at least one lowercase letter
+      return false unless @password.match?(/[a-z]/)
+  
+      # Check for at least one digit
+      return false unless @password.match?(/\d/)
+  
+      # Check for at least one special character
+      return false unless @password.match?(/[^A-Za-z0-9]/)
+  
+      # Check for maximum of 2 repeated characters
+      return false if @password.match?(/(.)\1{2,}/)
+  
+      true
+    end
+
+    def perform
+        validate
+    end
 end
