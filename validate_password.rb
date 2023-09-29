91fdead32307a400f04b1b212370685d39625c48
adrian_skowron <adrian_skowron@MacBook-Pro-adrian-skowron.local>
2023-09-29 11:59:09 +0200

    validate_password updated with other possible implementation

--- validate_password.rb
+++ validate_password.rb
@@ -17,13 +17,23 @@ class ValidatePassword
       return false unless @password.match?(/\d/)
   
       # Check for at least one special character
-      return false unless @password.match?(/[^A-Za-z0-9]/)
+      return false unless @password.match?(/[!@#\$%&*+=:;?<>]/)
   
       # Check for maximum of 2 repeated characters
       return false if @password.match?(/(.)\1{2,}/)
   
       true
     end
+
+=begin
+    def validation
+        if @password.length.between?(6, 24) && @password.match?(/[A-Z]/) && @password.match?(/[a-z]/) && @password.match?(/\d/) && @password.match?(/[!@#\$%&*+=:;?<>]/) && !@password.match?(/(.)\1{2,}/)
+            true
+        else
+            false
+        end
+    end
+=end
 
     def perform
         validate
